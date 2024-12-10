57a3a27c60c0fb28be3ef563e4a3aac0fdb98797
anon <anon@anon.anon>
2024-12-10 20:39:02 +0100

    Added 'Misc./terribly_old_scripts/bashrc-load.sh'

---
+++ Misc./terribly_old_scripts/bashrc-load.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+BASH_FOLDER="/home/jacob/Desktop/minecraft\ mod/Bash"
+sudo echo
+cp ${BASH_FOLDER}/.bashrc home/jacob/.bashrc
+echo "Bashrc loaded! Check your bash_aliases vim script, it might contain broken aliases!"
