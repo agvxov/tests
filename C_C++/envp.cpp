5043dd38034ff9da0cdb417fafbc003b0bf08976
anon <anon@anon.anon>
2025-12-04 11:57:24 +0100

    Added 'C_C++/envp.cpp'

---
+++ C_C++/envp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char* argv[], char* envp[]){
+}
+
+/*
+int main(int argc, char** argv, char** envp){
+
+	cout << strlen(*envp) << endl;
+
+	while(**envp){
+		cout << *envp++ << endl;
+	}
+
+	return 0;
+}*/
