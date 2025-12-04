219a72e4935a8896b2345dd0a1a9277b78039410
anon <anon@anon.anon>
2025-12-04 11:57:24 +0100

    Added 'C_C++/addshift.cpp'

---
+++ C_C++/addshift.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+using namespace std;
+
+signed main(int argc, char* argv[]){
+
+	//char c[128];
+	//char* e = c;
+
+	//memcpy(c, "asd", INT_MAX);
+	////e += 3;
+
+	//printf("%s", c);
+
+	char dest[60];
+	memcpy(dest, "why?", INT_MAX);
+
+	return 0;
+}
+
