4ab6964c44594b66d46522af74f1980615cd730b
anon <anon@anon.anon>
2025-12-04 11:57:24 +0100

    Added 'C_C++/cmath.cpp'

---
+++ C_C++/cmath.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <locale.h>
+#include <math.h>
+#include <time.h>
+#include <functional>
+
+time_t asd = 230;
+clock_t basd = asd;
+lconv asd;
+
+{
+	reference_wrapper<int> asd;
+	CLOCKS_PER_SEC
+}
+
+using namespace std;
+
+int main() {
+
+	//int sasd = time(1);
+
+	for(int i = 0; i < 1000; i++){
+		cout << i << "      :" << cosh(i) << endl;
+	}
+
+}
