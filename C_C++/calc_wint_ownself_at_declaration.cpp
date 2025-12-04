6cc3e9f0578737642b831b6910476f934a3c34f3
anon <anon@anon.anon>
2025-12-04 11:57:24 +0100

    Added 'C_C++/calc_wint_ownself_at_declaration.cpp'

---
+++ C_C++/calc_wint_ownself_at_declaration.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+signed main(int argc, char* argv[]){
+
+	int wtf = wtf + 3;		// how the hell does the compiler let you get away with this?!
+
+	cout << wtf << endl;
+
+	return 0;
+}
+
