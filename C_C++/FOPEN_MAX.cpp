689d4b45c83e6299a3c8e431d08c3cc3395e2801
anon <anon@anon.anon>
2025-12-04 11:57:23 +0100

    Added 'C_C++/FOPEN_MAX.cpp'

---
+++ C_C++/FOPEN_MAX.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cinttypes>
+#include <inttypes.h>
+#include <stdbool.h>
+
+#ifdef _____asdasdasdasd___i
+FOPEN_MAX
+TMP_MAX
+;
+PRId16
+PRId32
+PRId32
+PRId64
+maxdiv_t asd;
+#endif
+
+int main() {
+    return imaxabs();
+}
