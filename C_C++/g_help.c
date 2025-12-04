f6c9964d4e888e725234ef33429f2eb88593104a
anon <anon@anon.anon>
2025-12-04 11:57:24 +0100

    Added 'C_C++/g_help.c'

---
+++ C_C++/g_help.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+signed main(){
+	int exit = 0;
+	int sel = 0;
+	int input_decimal = 0;
+
+	while(exit == 0)
+	{
+		scanf("%d", &sel);
+
+		if(sel == 1)
+		{
+			printf("----Decimal to Binary----\n");
+			printf("Input a decimal number up to 4,294,967,295: \n");
+			scanf("%d", &input_decimal);
+			printf("Sneed");
+		}
+	}
+}
