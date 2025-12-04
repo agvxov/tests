4f41c5498aff8e23412cb53f61c0c4a9c84b81d4
anon <anon@anon.anon>
2025-12-04 11:57:25 +0100

    Added 'C_C++/map_to_pair.cpp'

---
+++ C_C++/map_to_pair.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+map<int, string> a = {{0, "asd"}, {1, "sdfg"}};
+
+signed main(){
+
+	cout << a[0];
+
+	return 0;
+}
