63d7a4e1d92b435955a0cce7b587003316f56d20
anon <anon@anon.anon>
2025-12-04 11:57:25 +0100

    Added 'C_C++/min.cpp'

---
+++ C_C++/min.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+signed main(int argc, char* argv[]){
+
+	int x = 3;
+	unsigned long y = 42;
+
+	cout << min((long long)x, (long long)y) << endl;
+
+	return 0;
+}
+
